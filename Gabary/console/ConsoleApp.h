2934dd40aabb9f83eccbaed049b4255a3b0c1183
Nemimeche Benaissa <nemimeche.benaissa@gmail.com>
2026-08-04 21:06:27 +0100

    Prepare Gabary Console Export Manager integration

--- Gabary/console/ConsoleApp.h
+++ Gabary/console/ConsoleApp.h
@@ -2,6 +2,7 @@
 #define GABARY_CONSOLE_APP_H
 
 #include <cstdint>
+#include <string>
 
 #include "SolarEngineV2.h"
 #include "ExportManager.h"
@@ -9,30 +10,53 @@
 #include "report/TemporalReportEngine.h"
 #include "report/TemporalReportJSON.h"
 
+
 class ConsoleApp
 {
 public:
+
     int run();
 
+
 private:
+
     void showBanner() const;
 
-    void showDay(int64_t dayId);
 
-    void shareDay(int64_t dayId);
+    void showDay(
+        int64_t dayId
+    );
 
-    void exportDay(int64_t dayId);
 
-    void inspectDay(int64_t dayId);
+    void shareDay(
+        int64_t dayId
+    );
 
-    void reportDay(int64_t dayId);
+
+    void exportDay(
+        int64_t dayId
+    );
+
+
+    void inspectDay(
+        int64_t dayId
+    );
+
+
+    void reportDay(
+        int64_t dayId
+    );
+
 
 private:
+
     Gabary::SolarEngineV2 solarEngine;
 
     TemporalReportEngine reportEngine;
 
     ExportManager exportManager;
+
 };
+
 
 #endif // GABARY_CONSOLE_APP_H
